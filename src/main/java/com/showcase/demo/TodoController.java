f033f83baf8833376fdea2e41e933e5e43dda082
Thiago Diniz da Silveira <thiagods.ti@gmail.com>
2022-04-22 19:43:12 +0200

    Add test for controller

--- src/main/java/com/showcase/demo/TodoController.java
+++ src/main/java/com/showcase/demo/TodoController.java
@@ -1,14 +1,18 @@
 package com.showcase.demo;
 
+import java.net.URI;
 import java.util.Optional;
 import lombok.AllArgsConstructor;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.util.UriComponentsBuilder;
 import reactor.core.publisher.Mono;
 import reactor.core.scheduler.Schedulers;
 
@@ -25,8 +29,10 @@ public class TodoController {
   }
 
   @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
-  private Mono<Void> save(@RequestBody Todo todo) {
-    repository.save(todo);
-    return Mono.empty();
+  private Mono<ResponseEntity<Void>> save(
+      @RequestBody Todo todo, UriComponentsBuilder componentsBuilder) {
+    Long id = repository.save(todo);
+    URI uri = componentsBuilder.path("/todos/{id}").buildAndExpand(id.toString()).toUri();
+    return Mono.just(ResponseEntity.created(uri).build());
   }
 }
